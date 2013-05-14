03da03524a3db97d6d00235a6a2f9569dcb99441
statuswoe <brentsmithis@gmail.com>
2013-05-13 23:13:31 -0300

    Adding DamageListener for explosions

---
+++ src/com/statuswoe/minecraft/DamageListener.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.statuswoe.minecraft;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+/**
+ * @author bsmith
+ *
+ */
+public class DamageListener implements Listener {
+
+	@EventHandler
+    public void onEntityDamage(EntityDamageEvent e){
+        if(e.getEntity() instanceof Player && e.getCause().equals(DamageCause.BLOCK_EXPLOSION)){
+            e.setCancelled(true);
+        }
+    }
+
+}
